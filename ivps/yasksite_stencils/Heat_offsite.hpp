94d816c5f3dd8e310fe172723af6a61495689a60
Christie Louis Alappat <christie.alappat@fau.de>
2020-04-20 18:35:41 +0200

    All input data used for the SC20 paper

---
+++ ivps/yasksite_stencils/Heat_offsite.hpp
@@ -0,0 +1,232 @@
+#ifndef _HEAT_OFFSITE_H
+#define _HEAT_OFFSITE_H
+
+//Implement various kinds of stencils required in the SeASiTe project.
+//If new stencils are required update this file
+#include "yask_compiler_api.hpp"
+using namespace std;
+using namespace yask;
+
+namespace {
+
+    class HeatBase_offsite : public yc_solution_with_radius_base {
+
+        protected:
+            // Indices & dimensions.
+            yc_index_node_ptr t = new_step_index("t");           // step in time dim.
+            yc_index_node_ptr x = new_domain_index("x");         // spatial dim.
+            yc_index_node_ptr y = new_domain_index("y");         // spatial dim.
+            yc_index_node_ptr z = new_domain_index("z");         // spatial dim.
+
+            // Vars.
+            yc_var_proxy data =
+                yc_var_proxy("data", get_soln(), { t, x, y, z }); // time-varying 3D var.
+
+            yc_var_proxy h_inv =
+                yc_var_proxy("h_inv", get_soln()); // time-varying 3D var.
+
+            // Return a coefficient.  Note: This returns completely fabricated
+            // values only for illustrative purposes; they have no mathematical
+            // significance.
+            virtual double coeff(int di, int dj, int dk) const {
+                return 1;
+            }
+
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& grid, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z) =0;
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& grid, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z) =0;
+            virtual void boundaryHandling(yc_var_proxy& grid, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z) =0;
+        public:
+
+            HeatBase_offsite(const string& name, int radius=1) : yc_solution_with_radius_base(name, radius) { }
+
+            // Define equation at t+1 based on values at t.
+            virtual void define() {
+
+                // A condition that is true when index 'x','y'or 'z' is at boundary.
+                // Normally bounding box calculation doesn't go to halo, but I hack
+                // it so that the same stencil can be used for all vector foldings
+                // without the problem of unimplemented unaligned writes
+                auto at_first_x = (x < first_domain_index(x));
+                auto at_first_y = (y < first_domain_index(y));
+                auto at_first_z = (z < first_domain_index(z));
+
+                auto at_last_x = (x > last_domain_index(x));
+                auto at_last_y = (y > last_domain_index(y));
+                auto at_last_z = (z > last_domain_index(z));
+
+                boundaryHandling(data, t,x,y,z, at_first_x, at_last_x, at_first_y, at_last_y, at_first_z, at_last_z);
+                yc_number_node_ptr v;
+                addPoints(v, data, t, x, y, z);
+                // define the value at t+1 to be equivalent to v.
+                data(t+1, x, y, z) EQUALS v;
+            }
+    };
+
+    class Heat1dStencil_offsite : public HeatBase_offsite {
+        protected:
+            virtual double coeff(int di, int dj, int dk) const
+            {
+                if(dk==0)
+                {
+                    return -2;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            // Add additional points to v.
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                for(int r=get_radius(); r >= 1; --r)
+                {
+                    v += coeff( 0, 0, r) * data(t, x, y, z+r  ) +
+                        coeff( 0, 0,-r) * data(t, x, y, z-r  );
+                }
+                v += coeff( 0, 0, 0) * data(t, x, y  , z  );
+
+            }
+
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                points(v, data, t, x, y, z);
+                v *= h_inv;
+            }
+
+            //Neumann boundary of first order
+            virtual void boundaryHandling(yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z)
+            {
+                //I do a hack here because unary '+' is not overloaded, adding
+                //another term data(t,x,y,z) is also a hack to show dependency to
+                //stencil compiler
+                data(t, x, y, z) EQUALS (data(t-1, x, y, z+1)*1.0)
+                    IF_DOMAIN at_first_z;
+
+                //I do a hack here because unary '+' is not overloaded
+                data(t, x, y, z) EQUALS (data(t-1, x, y, z-1)*1.0)
+                    IF_DOMAIN at_last_z;
+            }
+
+
+        public:
+            Heat1dStencil_offsite(int radius=1) :
+                HeatBase_offsite("Heat1D_offsite", radius) { }
+            Heat1dStencil_offsite(const string& name, int radius=1) :
+                HeatBase_offsite(name, radius) { }
+
+    };
+    YS_REGISTER_STENCIL_RADIUS("Heat1D_offsite",Heat1dStencil_offsite);
+
+    class Heat2dStencil_offsite : public Heat1dStencil_offsite {
+        protected:
+            virtual double coeff(int di, int dj, int dk) const
+            {
+                if(dj==0&&dk==0)
+                {
+                    return -4;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            // Add additional points to v.
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                for(int r=get_radius(); r>=1; --r)
+                {
+                    v += coeff( 0, r, 0) * data(t, x, y+r, z  ) +
+                        coeff( 0,-r, 0) * data(t, x, y-r, z  );
+                }
+
+                Heat1dStencil_offsite::points(v, data, t, x, y, z);
+
+            }
+
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                points(v, data, t, x, y, z);
+                v *= h_inv;
+            }
+
+            //Neumann boundary of first order
+            virtual void boundaryHandling(yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z)
+            {
+                /*    Heat1dStencil_offsite::boundaryHandling(data, t,x,y,z,at_first_x,at_last_x,at_first_y,at_last_y,at_first_z,at_last_z);
+
+                      data(t, x, y, z) EQUALS (data(t-1,x,y+1,z)*1.0)
+                      IF_DOMAIN at_first_y;
+
+                      data(t, x, y, z) EQUALS (data(t-1,x,y-1,z)*1.0)
+                      IF_DOMAIN at_last_y;
+                      */
+            }
+
+
+        public:
+            Heat2dStencil_offsite(int radius=1) :
+                Heat1dStencil_offsite("Heat2D_offsite", radius) { }
+            Heat2dStencil_offsite(const string& name, int radius=1) :
+                Heat1dStencil_offsite(name, radius) { }
+
+    };
+    YS_REGISTER_STENCIL_RADIUS("Heat2D_offsite",Heat2dStencil_offsite);
+
+
+    class Heat3dStencil_offsite : public Heat2dStencil_offsite {
+        protected:
+            virtual double coeff(int di, int dj, int dk) const
+            {
+                if((dj==0&&dk==0)&&(di==0))
+                {
+                    return -6;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            // Add additional points to v.
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                for(int r=get_radius(); r>=1; --r)
+                {
+                    v += coeff( r, 0, 0) * data(t, x+r, y, z ) +
+                        coeff(-r, 0, 0) * data(t, x-r, y, z );
+                }
+                Heat2dStencil_offsite::points(v, data, t, x, y, z);
+            }
+
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                points(v, data, t, x, y, z);
+                v*=h_inv;
+            }
+
+            //Neumann boundary of first order
+            virtual void boundaryHandling(yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z)
+            {
+                /*
+                   Heat2dStencil_offsite::boundaryHandling(data, t,x,y,z,at_first_x,at_last_x,at_first_y,at_last_y,at_first_z,at_last_z);
+                   data(t, x, y, z) EQUALS (data(t-1,x+1,y,z)*1.0)
+                   IF_DOMAIN at_first_x;
+                   data(t, x, y, z) EQUALS (data(t-1,x-1,y,z)*1.0)
+                   IF_DOMAIN at_last_x;
+                   */
+            }
+
+
+        public:
+            Heat3dStencil_offsite(int radius=1) :
+                Heat2dStencil_offsite("Heat3D_offsite", radius) { }
+            Heat3dStencil_offsite(const string& name, int radius=1) :
+                Heat2dStencil_offsite(name, radius) { }
+
+    };
+    YS_REGISTER_STENCIL_RADIUS("Heat3D_offsite",Heat3dStencil_offsite);
+}
+
+#endif
---
+++ ivps/yasksite_stencils/Wave_offsite.hpp
@@ -0,0 +1,222 @@
+#ifndef _WAVE_OFFSITE_H
+#define _WAVE_OFFSITE_H
+//Implement various kinds of stencils required in the SeASiTe project.
+//If new stencils are required update this file
+#include "yask_compiler_api.hpp"
+using namespace std;
+using namespace yask;
+
+namespace {
+    class WaveBase_offsite : public yc_solution_with_radius_base {
+
+        protected:
+
+            // Indices & dimensions.
+            yc_index_node_ptr t = new_step_index("t");           // step in time dim.
+            yc_index_node_ptr x = new_domain_index("x");         // spatial dim.
+            yc_index_node_ptr y = new_domain_index("y");         // spatial dim.
+            yc_index_node_ptr z = new_domain_index("z");         // spatial dim.
+
+            // Vars.
+            yc_var_proxy data =
+                yc_var_proxy("data", get_soln(), { t, x, y, z }); // time-varying 3D var.
+
+            yc_var_proxy vel  =
+                yc_var_proxy("vel", get_soln(), {t, x, y, z }); // 3D var.
+
+
+            yc_var_proxy prev_data  =
+                yc_var_proxy("prev_data", get_soln(), {t, x, y, z }); // 3D var.
+
+
+            // Return a coefficient.  Note: This returns completely fabricated
+            // values only for illustrative purposes; they have no mathematical
+            // significance.
+            virtual double coeff(int di, int dj, int dk) const {
+                return 1;
+            }
+
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& grid, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z) =0;
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& grid, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z) =0;
+            virtual void boundaryHandling(yc_var_proxy& grid, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z) =0;
+        public:
+
+            WaveBase_offsite(const string& name, int radius=4) : yc_solution_with_radius_base(name, radius) {
+                //can support max. upto 3D space dimension
+            }
+
+            // Define equation at t+1 based on values at t.
+            virtual void define() {
+
+                // A condition that is true when index 'x','y'or 'z' is at boundary.
+                // Normally bounding box calculation doesn't go to halo, but I hack
+                // it so that the same stencil can be used for all vector foldings
+                // without the problem of unimplemented unaligned writes
+                const yc_bool_node_ptr& at_first_x = (x < first_domain_index(x));
+                const yc_bool_node_ptr& at_first_y = (y < first_domain_index(y));
+                const yc_bool_node_ptr& at_first_z = (z < first_domain_index(z));
+
+                const yc_bool_node_ptr& at_last_x = (x > last_domain_index(x));
+                const yc_bool_node_ptr& at_last_y = (y > last_domain_index(y));
+                const yc_bool_node_ptr& at_last_z = (z > last_domain_index(z));
+
+                boundaryHandling(data, t,x,y,z, at_first_x, at_last_x, at_first_y, at_last_y, at_first_z, at_last_z);
+                yc_number_node_ptr v;
+                points(v, data, t, x, y, z);
+                // define the value at t+1 to be equivalent to v.
+                double c_0 = 0.1;
+                yc_number_node_ptr tmp;
+                tmp = c_0*data(t,x,y,z) + v;
+                data(t+1, x, y, z) EQUALS 2*data(t,x,y,z) - prev_data(t,x,y,z) + vel(t,x,y,z)*tmp;
+            }
+    };
+
+    class Wave1dStencil_offsite : public WaveBase_offsite {
+        protected:
+            virtual double coeff(int dr) const
+            {
+                return 1/(double)dr;
+            }
+
+            // Add additional points to v.
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                for(int r=get_radius(); r >= 1; --r)
+                {
+                    v += coeff(r) * (data(t, x, y, z+r  ) +  data(t, x, y, z-r  ));
+                }
+
+            }
+
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                points(v, data, t, x, y, z);
+                double c_0 = 0.1;
+                yc_number_node_ptr tmp;
+                tmp = c_0*data(t,x,y,z) + v;
+                v += 2*data(t,x,y,z) - prev_data(t,x,y,z) + vel(t,x,y,z)*tmp;
+
+            }
+            //Neumann boundary of first order
+            virtual void boundaryHandling(yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z)
+            {
+                //I do a hack here because unary '+' is not overloaded, adding
+                //another term data(t,x,y,z) is also a hack to show dependency to
+                //stencil compiler
+                data(t, x, y, z) EQUALS (data(t-1, x, y, z+1)*1.0)
+                    IF_DOMAIN at_first_z;
+
+                //I do a hack here because unary '+' is not overloaded
+                data(t, x, y, z) EQUALS (data(t-1, x, y, z-1)*1.0)
+                    IF_DOMAIN at_last_z;
+            }
+
+
+        public:
+            Wave1dStencil_offsite(int radius=4) :
+                WaveBase_offsite("Wave1D_offsite", radius) { }
+            Wave1dStencil_offsite(const string& name, int radius=4) :
+                WaveBase_offsite(name, radius) { }
+
+    };
+    YS_REGISTER_STENCIL_RADIUS("Wave1D_offsite",Wave1dStencil_offsite);
+
+    class Wave2dStencil_offsite : public Wave1dStencil_offsite {
+        protected:
+            virtual double coeff(int dr) const
+            {
+                return 1/(double)dr;
+            }
+
+            // Add additional points to v.
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                for(int r=get_radius(); r>=1; --r)
+                {
+                    v += coeff( r ) * (data(t, x, y+r, z  ) + data(t, x, y-r, z  ));
+                }
+                Wave1dStencil_offsite::points(v, data, t, x, y, z);
+            }
+
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                points(v, data, t, x, y, z);
+                double c_0 = 0.1;
+                yc_number_node_ptr tmp;
+                tmp = c_0*data(t,x,y,z) + v;
+                v += 2*data(t,x,y,z) - prev_data(t,x,y,z) + vel(t,x,y,z)*tmp;
+            }
+
+            //Neumann boundary of first order
+            virtual void boundaryHandling(yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z)
+            {
+                    Wave1dStencil_offsite::boundaryHandling(data, t,x,y,z,at_first_x,at_last_x,at_first_y,at_last_y,at_first_z,at_last_z);
+
+                      data(t, x, y, z) EQUALS (data(t-1,x,y+1,z)*1.0)
+                      IF_DOMAIN at_first_y;
+
+                      data(t, x, y, z) EQUALS (data(t-1,x,y-1,z)*1.0)
+                      IF_DOMAIN at_last_y;
+            }
+
+
+        public:
+            Wave2dStencil_offsite(int radius=4) :
+                Wave1dStencil_offsite("Wave2D_offsite", radius) { }
+            Wave2dStencil_offsite(const string& name, int radius=4) :
+                Wave1dStencil_offsite(name, radius) { }
+
+    };
+    YS_REGISTER_STENCIL_RADIUS("Wave2D_offsite",Wave2dStencil_offsite);
+
+
+    class Wave3dStencil_offsite : public Wave2dStencil_offsite {
+        protected:
+            virtual double coeff(int dr) const
+            {
+                return 1/(double)dr;
+            }
+
+            // Add additional points to v.
+            virtual void points(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                for(int r=get_radius(); r>=1; --r)
+                {
+                    v += coeff( r ) * (data(t, x+r, y, z ) + data(t, x-r, y, z ));
+                }
+                Wave2dStencil_offsite::points(v, data, t, x, y, z);
+            }
+
+            virtual void addPoints(yc_number_node_ptr& v, yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z)
+            {
+                points(v, data, t, x, y, z);
+                double c_0 = 0.1;
+                yc_number_node_ptr tmp;
+                tmp = c_0*data(t,x,y,z) + v;
+                v += 2*data(t,x,y,z) - prev_data(t,x,y,z) + vel(t,x,y,z)*tmp;
+            }
+
+            //Neumann boundary of first order
+            virtual void boundaryHandling(yc_var_proxy& data, const yc_number_node_ptr& t, const yc_number_node_ptr& x, const yc_number_node_ptr& y, const yc_number_node_ptr& z, const yc_bool_node_ptr& at_first_x, const yc_bool_node_ptr& at_last_x, const yc_bool_node_ptr& at_first_y, const yc_bool_node_ptr& at_last_y, const yc_bool_node_ptr& at_first_z, const yc_bool_node_ptr& at_last_z)
+            {
+/*                   Wave2dStencil_offsite::boundaryHandling(data, t,x,y,z,at_first_x,at_last_x,at_first_y,at_last_y,at_first_z,at_last_z);
+                   data(t, x, y, z) EQUALS (data(t-1,x+1,y,z)*1.0)
+                   IF_DOMAIN at_first_x;
+                   data(t, x, y, z) EQUALS (data(t-1,x-1,y,z)*1.0)
+                   IF_DOMAIN at_last_x;
+                   */
+            }
+
+
+        public:
+            Wave3dStencil_offsite(int radius=4) :
+                Wave2dStencil_offsite("Wave3D_offsite", radius) { }
+            Wave3dStencil_offsite(const string& name, int radius=4) :
+                Wave2dStencil_offsite(name, radius) { }
+
+    };
+
+    YS_REGISTER_STENCIL_RADIUS("Wave3D_offsite",Wave3dStencil_offsite);
+}
+
+#endif
